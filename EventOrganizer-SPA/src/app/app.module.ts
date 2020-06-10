51ded19dfdaf5ea04021e3e4283833b65e7052c2
milosnikic <milos.nikic@gmail.com>
2020-06-11 01:19:57 +0200

    - Added calendar component and dependencies

--- EventOrganizer-SPA/src/app/app.module.ts
+++ EventOrganizer-SPA/src/app/app.module.ts
@@ -3,13 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule  } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
  
 import { ToastrModule } from 'ngx-toastr';
 import { AppRoutingModule } from './app-routing.module';
 import { CalendarComponent } from './calendar/calendar.component';
+import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
+import { FlatpickrModule } from 'angularx-flatpickr';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
    declarations: [
@@ -18,10 +23,18 @@ import { CalendarComponent } from './calendar/calendar.component';
       CalendarComponent,
    ],
    imports: [
+      CommonModule,
       HttpClientModule,
       ReactiveFormsModule,
       BrowserModule,
       BrowserAnimationsModule,
+      FormsModule,
+      NgbModalModule,
+      FlatpickrModule.forRoot(),
+      CalendarModule.forRoot({
+        provide: DateAdapter,
+        useFactory: adapterFactory,
+      }),
       ToastrModule.forRoot(
          {
             timeOut: 5000,
--- EventOrganizer-SPA/src/app/calendar/calendar.component.ts
+++ EventOrganizer-SPA/src/app/calendar/calendar.component.ts
@@ -1,15 +1,117 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ChangeDetectionStrategy,
+  ViewChild,
+  TemplateRef,
+} from '@angular/core';
+import {
+  startOfDay,
+  endOfDay,
+  subDays,
+  addDays,
+  endOfMonth,
+  isSameDay,
+  isSameMonth,
+  addHours,
+} from 'date-fns';
+import { Subject } from 'rxjs';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import {
+  CalendarEvent,
+  CalendarEventAction,
+  CalendarEventTimesChangedEvent,
+  CalendarView,
+} from 'angular-calendar';
+import { FormBuilder, Validators } from '@angular/forms';
+
+const colors: any = {
+  red: {
+    primary: '#ad2121',
+    secondary: '#FAE3E3',
+  },
+  blue: {
+    primary: '#1e90ff',
+    secondary: '#D1E8FF',
+  },
+  yellow: {
+    primary: '#e3bc08',
+    secondary: '#FDF1BA',
+  },
+};
 
 @Component({
   selector: 'app-calendar',
   templateUrl: './calendar.component.html',
-  styleUrls: ['./calendar.component.css']
+  styleUrls: ['./calendar.component.css'],
 })
-export class CalendarComponent implements OnInit {
+export class CalendarComponent {
+  @ViewChild('modalContent', { static: true }) modalContent: TemplateRef<any>;
 
-  constructor() { }
+  newEventForm = this.fb.group({
+    title:['',[Validators.required]],
+    primaryColor:[colors.red.primary],
+    secondaryColor:[colors.red.primary],
+    startDate:[Date.now()],
+    endDate:[Date.now()]
+  })
 
-  ngOnInit() {
+  view: CalendarView = CalendarView.Month;
+
+  CalendarView = CalendarView;
+
+  viewDate: Date = new Date();
+
+  event: any;
+
+  refresh: Subject<any> = new Subject();
+
+  events: CalendarEvent[] = [];
+
+  activeDayIsOpen: boolean = true;
+
+  constructor(private fb: FormBuilder) {}
+
+  dayClicked({ date, events }: { date: Date; events: CalendarEvent[] }): void {
+    if (isSameMonth(date, this.viewDate)) {
+      if (
+        (isSameDay(this.viewDate, date) && this.activeDayIsOpen === true) ||
+        events.length === 0
+      ) {
+        this.activeDayIsOpen = false;
+      } else {
+        this.activeDayIsOpen = true;
+      }
+      this.viewDate = date;
+    }
   }
 
+  addEvent(): void {
+    this.event = {};
+    this.event.title = this.newEventForm.get('title').value;
+    this.event.color = {
+      primary: this.newEventForm.get('primaryColor').value,
+      secondary: this.newEventForm.get('secondaryColor').value
+    };
+    this.event.start = new Date(this.newEventForm.get('startDate').value);
+    this.event.end = new Date(this.newEventForm.get('endDate').value);
+
+
+    this.events = [
+      ...this.events,
+      this.event
+    ];
+  }
+
+  deleteEvent(eventToDelete: CalendarEvent) {
+    this.events = this.events.filter((event) => event !== eventToDelete);
+  }
+
+  setView(view: CalendarView) {
+    this.view = view;
+  }
+
+  closeOpenMonthViewDay() {
+    this.activeDayIsOpen = false;
+  }
 }
